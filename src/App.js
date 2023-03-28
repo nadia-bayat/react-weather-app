9e86e74b218928b106fe34bdd53361be24a07a8d
nadia-bayat <121291038+nadia-bayat@users.noreply.github.com>
2023-03-28 12:50:14 +0330

    Added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <footer>
+        This application is coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/nadia-bayat-9162b9222/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Nadia Bayat
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/nadia-bayat/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
