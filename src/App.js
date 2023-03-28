b37e125619104318ace71da81a40323c0d2d10ff
nadia-bayat <121291038+nadia-bayat@users.noreply.github.com>
2023-03-28 13:02:48 +0330

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This application is coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/nadia-bayat-9162b9222/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Nadia Bayat
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/nadia-bayat/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This application is coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nadia-bayat-9162b9222/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nadia Bayat
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/nadia-bayat/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
