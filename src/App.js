ba1c0410141e2d176a80efbe19e2411acbe3968a
nadia-bayat <121291038+nadia-bayat@users.noreply.github.com>
2023-03-28 13:53:57 +0330

    Created weather HTML structure & CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+import React from "react";
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
         <footer>
-          This application is coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/nadia-bayat-9162b9222/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Nadia Bayat
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/nadia-bayat/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
+          <small>
+            This project is coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/nadia-bayat-9162b9222/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Nadia Bayat
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/nadia-bayat/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </small>
         </footer>
       </div>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <div className="Weather-app-wrapper">
+        <div className="Weather-app">
+          <form id="search-form" className="mb-4">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  id="search-city"
+                  type="search"
+                  placeholder="Type a city..."
+                  autofocus="on"
+                  autocomplete="off"
+                  className="form-control"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <h1 id="city">Rome</h1>
+          <ul>
+            <li id="dateTime">December 28, 12:26</li>
+            <li id="description"></li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+              <div className="d-flex Weather-temperature">
+                <img src="" alt="Clear" id="icon" />
+                <div>
+                  <strong id="temperature"></strong>
+                  <span className="units">
+                    <a id="cLink" href="#">
+                      °C
+                    </a>
+                    |
+                    <a id="fLink" href="#">
+                      °F
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  Humidity: <span id="humidity"></span>%
+                </li>
+                <li>
+                  Wind: <span id="wind"> </span> km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="weather-forecast" id="forecast"></div>
+      </div>
+    </div>
+  );
+}
