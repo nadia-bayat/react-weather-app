0c2d4a8815fe64ee14bd6482f73d5fccff751471
nadia-bayat <121291038+nadia-bayat@users.noreply.github.com>
2023-04-04 12:13:25 +0330

    Added Netlify URL

--- src/App.js
+++ src/App.js
@@ -23,6 +23,14 @@ export default function App() {
               rel="noreferrer"
             >
               open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://stellular-youtiao-aad217.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hosted on Netlify
             </a>
           </small>
         </footer>
